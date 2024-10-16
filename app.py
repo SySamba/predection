a7ee07aa460295212125d02f471df41e7b47f38a
SySamba <sambasy837@gmail.com>
2024-10-16 21:22:25 +0000

    includion des dependances

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
 # Charger les modèles pré-entraînés
-model_cancer = load_model('model_breast_cancer.h5')
-model_diabetes = load_model('mon_modele_diabete.h5')
+model_cancer = load_model(os.path.join('flask', 'model_breast_cancer.h5'))
+model_diabetes = load_model(os.path.join('flask', 'mon_modele_diabete.h5'))
 
 # Scalers pour normaliser les données (ceux utilisés lors de l'entraînement)
+# Assure-toi que ces scalers ont été ajustés avec les mêmes données que celles utilisées lors de l'entraînement
 scaler_cancer = StandardScaler()
 scaler_diabetes = StandardScaler()
 
@@ -29,7 +30,7 @@ def predict_cancer():
     
     # Convertir les données en numpy array et normaliser
     input_data = np.array(input_data).reshape(1, -1)
-    input_data_scaled = scaler_cancer.fit_transform(input_data)  # Appliquer le scaler
+    input_data_scaled = scaler_cancer.transform(input_data)  # Appliquer uniquement transform
     
     # Faire la prédiction
     prediction = model_cancer.predict(input_data_scaled)[0][0]
@@ -44,7 +45,7 @@ def predict_diabetes():
     
     # Convertir les données en numpy array et normaliser
     input_data = np.array(input_data).reshape(1, -1)
-    input_data_scaled = scaler_diabetes.fit_transform(input_data)  # Appliquer le scaler
+    input_data_scaled = scaler_diabetes.transform(input_data)  # Appliquer uniquement transform
     
     # Faire la prédiction
     prediction = model_diabetes.predict(input_data_scaled)[0][0]
